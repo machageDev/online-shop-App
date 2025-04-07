caa1328d2b97657f777967839604cefca8f25e91
machageDev <machagefranklyn@gmail.com>
2025-04-06 23:59:01 -1200

    great

--- UrbanFit/views.py
+++ UrbanFit/views.py
@@ -7,3 +7,9 @@ def login_view(request):
     return render(request, 'login.html')  # or 'UrbanFit/login.html' if inside app folder
 def navbar(request):
     return render(request,'navbar.html')
+def home(request):
+    return render(request,'home.html')
+def forgot_password(request):
+    return render(request,'forgot_password.html')
+def register(request):
+    return render(request,'register.html')
