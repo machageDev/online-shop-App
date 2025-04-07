553a4a4cb09d28bad40e601bcd826a7fd89e6acc
machageDev <machagefranklyn@gmail.com>
2025-04-07 07:07:49 -1200

    modified models

--- UrbanFit/models.py
+++ UrbanFit/models.py
@@ -4,10 +4,9 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    email = models.EmailField(unique=True,max_length=254)
+    email = models.EmailField(unique=True, max_length=254)
     password = models.CharField(max_length=200)
-    user = models.ForeignKey=models.CASCADE
     
     def __str__(self):
-        return self.email
+        return f"{self.first_name} {self.last_name}"
     
