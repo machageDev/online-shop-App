b0e2b49da9e6ae1c2425eaaab6f22c1b07876dd5
machageDev <machagefranklyn@gmail.com>
2025-04-06 23:01:07 -1200

    login solved

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -123,7 +124,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
