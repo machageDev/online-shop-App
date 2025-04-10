200548aefef49b77ca6395ebb671ae55e110589c
machageDev <machagefranklyn@gmail.com>
2025-04-09 23:56:51 -1200

    added class product

--- UrbanFit/models.py
+++ UrbanFit/models.py
@@ -10,3 +10,25 @@ class User(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
     
+class Product(models.Model):
+        CATEGORY_CHOICES = [
+      
+            ('men','Men'),
+            ('women','Women'),
+            ('kids','Kids'),
+            ('shoes','Shoes'),
+            ('accessories','Accessories'),
+        ]
+        name = models.CharField(max_length=200)
+        description = models.TextField(max_length=200)
+        price = models.DecimalField(max_digits=10, decimal_places=2)
+        category = models.CharField(max_length=11, choices=CATEGORY_CHOICES)
+        image = models.ImageField(upload_to='products/')
+        color = models.CharField(max_length=200)
+        
+        def __str__(self):
+            return self.name
+            
+    
+    
+   
