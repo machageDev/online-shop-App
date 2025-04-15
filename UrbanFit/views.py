123e373eb98e006d094a9fdc0cd8a8134961dcc3
machageDev <machagefranklyn@gmail.com>
2025-04-15 03:37:43 -1200

    new modifications /otp.html,reset_password.html

--- UrbanFit/views.py
+++ UrbanFit/views.py
@@ -1,6 +1,9 @@
 from pyexpat.errors import messages
+import random
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
+
+from Eshop import settings
 from .models import Product
 from django.core.paginator import Paginator
 
@@ -52,7 +55,7 @@ def register(request):
         messages.success(request,"user registered successfully")
         return redirect('login')
     return render(request,'register.html')
-        
+@login_required        
 def product_listing(request):
     # Initialize filters with default values
     category_filter = request.GET.get('category', None)
@@ -94,4 +97,49 @@ def product_listing(request):
     }
     
     return render(request, 'product_listing.html', context)
+def send_otp(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        user = User.objects.filter(email=email).first()
+        if user:
+            otp = str(random.randint(100000, 999999))
+            request.session['otp'] = otp
+            request.session['email'] = email
+            send_mail(
+                'password Rest OTP',
+                f'Your OTP is {otp}',
+                settings.DEFAULT_FROM_EMAIL,
+                [email],
+                fail_silently=False,
                 
+            )
+            messages.success(request,'OTP sent to your email')
+            return redirect('verify_otp')
+        
+def otp(request):
+    if request.method == 'POST':
+        entered_otp = request.POST.get('otp')
+        stored_otp = request.session.get('otp')
+        if stored_otp and str(entered_otp) == str(stored_otp):
+            return redirect('reset_password')  # Redirect to password reset page
+        else:
+            messages.error(request, 'Invalid OTP. Please try again.')
+
+    return render(request, 'otp.html')
+def reset_password(request):
+    if request.method == 'POST':
+        new_password = request.POST.get('password')
+        email = request.session.get('email')
+
+        if email:
+            user = get_user_model().objects.filter(email=email).first()
+            if user:
+                user.set_password(new_password)
+                user.save()
+                messages.success(request, 'Password reset successfully. You can now log in.')
+                return redirect('login')
+
+        messages.error(request, 'Error resetting password.')
+
+    return render(request, 'reset_password.html')        
+                 
